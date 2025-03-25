55bf56c83cef31376a24fcf7d8082e45ff3fa075
xeffen25 <xeffen25@xeffen25.com>
2025-03-25 19:47:50 +0000

    Development Environments - Fixes #2

--- prettierrc.mjs
+++ prettierrc.mjs
@@ -1,5 +1,9 @@
 /** @type {import("prettier").Config} */
 export default {
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: false,
   plugins: ["prettier-plugin-astro"],
   overrides: [
     {
