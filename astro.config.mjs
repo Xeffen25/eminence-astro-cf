0c6417f38dfe461fe1c0c18351319c4b843302f2
xeffen25 <xeffen25@xeffen25.com>
2025-03-26 10:16:18 +0000

    Localization - Fixes #5

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,10 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://eminence-astro-cf.xeffen25.workers.dev/",
+  i18n: {
+    locales: ["es"],
+    defaultLocale: "es",
+  },
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
