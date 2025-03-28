1d15997d9b71419b26671e8365ad8e9bfb0fe19a
xeffen25 <xeffen25@xeffen25.com>
2025-03-28 12:21:50 +0000

    Feat(Sitemap): Added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,25 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import cloudflare from "@astrojs/cloudflare";
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://eminence-astro-cf.xeffen25.workers.dev/",
 	i18n: {
-		locales: ["es"],
 		defaultLocale: "es",
+		locales: ["es"],
 	},
+
+	output: "server",
 	adapter: cloudflare({
 		platformProxy: {
 			enabled: true,
 		},
 	}),
-	output: "server",
 	vite: {
 		plugins: [tailwindcss()],
 	},
+	integrations: [sitemap()],
 });
